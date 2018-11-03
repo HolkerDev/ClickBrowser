65e8acb7ba96ad863ce68e38ecffc5fdd11e4ce4
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-03 13:40:52 +0100

    Add integrated web browser

--- app/src/main/java/com/dev/holker/clickbrowser/MainActivity.kt
+++ app/src/main/java/com/dev/holker/clickbrowser/MainActivity.kt
@@ -2,11 +2,18 @@ package com.dev.holker.clickbrowser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.settings.loadsImagesAutomatically = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.google.com/")
     }
 }
+
