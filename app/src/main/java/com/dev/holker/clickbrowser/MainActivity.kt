f0c9ca530a04b364a96df440e771a8b664b3ff29
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-03 14:55:38 +0100

    Add buttons 'back' & 'forward'

--- app/src/main/java/com/dev/holker/clickbrowser/MainActivity.kt
+++ app/src/main/java/com/dev/holker/clickbrowser/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.dev.holker.clickbrowser
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_creator,menu)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
