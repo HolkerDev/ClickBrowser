cae53edc8ee14a071cb037e119a4a807b3f632f2
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-11 16:36:53 +0100

    Add AlertDialog to bookmark activity

--- app/src/main/java/com/dev/holker/clickbrowser/Bookmarks.kt
+++ app/src/main/java/com/dev/holker/clickbrowser/Bookmarks.kt
@@ -1,7 +1,9 @@
 package com.dev.holker.clickbrowser
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_bookmarks.*
 
 class Bookmarks : AppCompatActivity() {
@@ -10,6 +12,18 @@ class Bookmarks : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bookmarks)
         val intent = intent
-        textView.text = intent.getStringExtra("web")
+        val prevWeb: String = intent.getStringExtra("web")
+
+        AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_star_black_24dp)
+            .setTitle("Choose")
+            .setMessage("Do want to add $prevWeb to bookmarks ?")
+            .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
+                run {
+                    textView.text = prevWeb
+                }
+            }
+            .setNegativeButton("No", null)
+            .show()
     }
 }
