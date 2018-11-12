6bca5efe1c1d87c3e95c4d6c56e17c444a86a29e
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-12 17:56:19 +0100

    Add bookmark list

--- app/src/main/java/com/dev/holker/clickbrowser/Bookmarks.kt
+++ app/src/main/java/com/dev/holker/clickbrowser/Bookmarks.kt
@@ -2,22 +2,47 @@ package com.dev.holker.clickbrowser
 
 import android.content.Context
 import android.content.DialogInterface
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteDatabaseLockedException
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_bookmarks.*
 
 class Bookmarks : AppCompatActivity() {
 
+    lateinit var SQLDatabase: SQLiteDatabase
+    lateinit var ArrayAdapter: ArrayAdapter<String>
+    val BookmarksList = arrayListOf<String>()
 
     //check if DB exists. If not - create its
     fun creatingDB() {
-        val sqlDataBase: SQLiteDatabase = this.openOrCreateDatabase("Bookmarks", Context.MODE_PRIVATE, null)
-        sqlDataBase.execSQL("CREATE TABLE IF NOT EXISTS bookmarks (webPage VARCHAR)")
+        SQLDatabase = this.openOrCreateDatabase("Bookmarks", Context.MODE_PRIVATE, null)
+        SQLDatabase.execSQL("CREATE TABLE IF NOT EXISTS bookmarks (webPage VARCHAR)")
     }
-    
+
+
+    fun addBookmark(webAddress: String) {
+        val exec: String = "INSERT INTO bookmarks (webPage) VALUES ('$webAddress')"
+        SQLDatabase.execSQL(exec)
+    }
+
+    fun update() {
+        val c: Cursor = SQLDatabase.rawQuery("SELECT DISTINCT * FROM bookmarks", null)
+
+        val bookmarksIndex: Int = c.getColumnIndex("webPage")
+
+        c.moveToFirst()
+        while (!c.isAfterLast()) {
+            BookmarksList.add(c.getString(bookmarksIndex))
+            //Log.i("MyLog", c.getString(bookmarksIndex))
+            c.moveToNext()
+        }
+        lv_bookmarks.adapter = ArrayAdapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +50,10 @@ class Bookmarks : AppCompatActivity() {
 
         creatingDB()
 
-
         val intent = intent
         val prevWeb: String = intent.getStringExtra("web")
-        val bookmarks = arrayListOf<String>()
 
-        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bookmarks)
+        ArrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, BookmarksList)
 
         AlertDialog.Builder(this)
             .setIcon(R.drawable.ic_star_black_24dp)
@@ -38,12 +61,15 @@ class Bookmarks : AppCompatActivity() {
             .setMessage("Do want to add $prevWeb to bookmarks ?")
             .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
                 run {
-                    bookmarks.add(prevWeb)
+                    addBookmark(prevWeb)
+                    update()
                 }
             }
-            .setNegativeButton("No", null)
+            .setNegativeButton("No") { dialog: DialogInterface?, which: Int ->
+                run {
+                    update()
+                }
+            }
             .show()
-
-        lv_bookmarks.adapter = arrayAdapter
     }
 }
